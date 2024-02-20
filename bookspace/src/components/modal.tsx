0f9750e1d4f456babde82636e19697a549ddf50e
Hubert Lin <linhub15@gmail.com>
2024-02-20 09:42:11 -0700

    fix: calendar overflow hidden

--- bookspace/src/components/modal.tsx
+++ bookspace/src/components/modal.tsx
@@ -41,7 +41,7 @@ export function Modal(
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative w-full max-w-xl transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:p-6">
+              <Dialog.Panel className="relative w-full max-w-xl transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:p-6">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
