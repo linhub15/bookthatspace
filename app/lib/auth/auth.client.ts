2cc0a2f176cec9e802e983e8a50f4494a7045245
Hubert Lin <linhub15@users.noreply.github.com>
2025-03-07 17:16:22 -0700

    fix: pass in BaseUrl for betterAuthClient

--- app/lib/auth/auth.client.ts
+++ app/lib/auth/auth.client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000/api/auth", // auth server
+  baseURL: import.meta.env.VITE_AUTH_URL, // auth server
 });
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,10 @@
 declare const VITE_APP_VERSION: string;
+
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_AUTH_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
